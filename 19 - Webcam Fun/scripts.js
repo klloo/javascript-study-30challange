8299cf8333c6d0a778557d6f9b25c3f1f971d096
klloo <asdf016182@naver.com>
2022-01-30 22:00:09 +0900

    [Day 19] webcam (미완)

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,37 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    .then((content) => {
+        video.srcObject = content;
+        video.play();
+    })
+    .catch(e => {
+        throw new Error(e);
+    });
+}
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+    return setInterval(()=>{
+        ctx.drawImage(video, 0, 0, width, height);
+        // take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // mess with them
+        // pixels = redEffect(pixels);
+    
+        pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+    
+        // pixels = greenScreen(pixels);
+        // put them back
+        ctx.putImageData(pixels, 0, 0);
+    },16);
+}
+
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
